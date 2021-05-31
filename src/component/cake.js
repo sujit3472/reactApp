f1492da1d817b0500480eb085033f426ee6ffb34
Sujit Shinde <sujit3472shinde@gmail.com>
2021-05-31 12:35:55 +0530

    conditional render stmt changed

--- src/component/cake.js
+++ src/component/cake.js
@@ -4,10 +4,9 @@ function Cake(props) {
 	
 	if(props.data) {
 		//console.log("props.data", props.data);	
-
 		return (
 			<div className="card m-2" style={{width: "18rem" }}>
-				{props.data.image  ?  <img style={{height: "15rem" }} src={props.data.image} className="card-img-top" alt="" /> : <img style={{height: "15rem" }} src="No_picture_available.png" className="card-img-top" alt="No Image" /> }
+				<img style={{height: "15rem" }} src={props.data.image ? props.data.image : "No_picture_available.png"} className="card-img-top" alt="" /> 
 				<div className="card-body">
 				<h5 className="card-title">{props.data.name }</h5>
 				<p> {props.data.price}</p>
